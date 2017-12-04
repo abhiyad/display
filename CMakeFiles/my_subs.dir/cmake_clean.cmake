0e5bce9e6a839c9e4a7acec79da77eb6d5616586
Abhishek Yadav <abhishekcms611@gmail.com>
2017-12-04 21:33:02 +0530

    display image package

---
+++ CMakeFiles/my_subs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_subs.dir/src/my_subs.cpp.o"
+  "devel/lib/display/my_subs.pdb"
+  "devel/lib/display/my_subs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_subs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/subs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subs.dir/src/subs.cpp.o"
+  "devel/lib/display/subs.pdb"
+  "devel/lib/display/subs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
